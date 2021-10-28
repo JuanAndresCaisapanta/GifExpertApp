d267b092fb69957f5ebfec16cbf479fb83055469
Juan Caisapanta <juancaisapanta@hotmail.com>
2021-10-28 11:42:48 -0500

    terminado

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+
+export const getGifs = async (category) => {
+    const url =
+      `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=QrCYsMs7OENWI7a7Da4PE392u2KJEQbU`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+  return gifs;
+}
